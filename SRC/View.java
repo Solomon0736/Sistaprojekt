962c37e3626fbe787a72de6546259232b6ac08e6
solomontikabo.musie <Nordmaling10>
2021-05-03 11:35:35 +0200

    Jag har ritat projektet i diagreams.net och har skrev mina planering

--- SRC/View.java
+++ SRC/View.java
@@ -1,60 +1,30 @@
 import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.awt.event.ComponentAdapter;
 import java.awt.event.ComponentEvent;
 
 public class View {
-    private JLabel name;
-    private JLabel word;
-    private JTextField textField1;
-    private JPasswordField passwordField1;
-    private JPanel Backgrund;
-
-
-    public JLabel getName() {
-        return name;
-    }
-
-    public void setName(JLabel name) {
-        this.name = name;
-    }
-
-    public JLabel getWord() {
-        return word;
-    }
-
-    public void setWord(JLabel word) {
-        this.word = word;
-    }
-
-    public JTextField getTextField1() {
-        return textField1;
-    }
-
-    public void setTextField1(JTextField textField1) {
-        this.textField1 = textField1;
-    }
-
-    public JPasswordField getPasswordField1() {
-        return passwordField1;
-    }
-
-    public void setPasswordField1(JPasswordField passwordField1) {
-        this.passwordField1 = passwordField1;
-    }
+        private JLabel name;
+        private JLabel word;
+        private JTextField textField1;
+        private JPasswordField passwordField1;
+        private JPanel Backgrund;
 
     public View() {
+            JFrame frame = new JFrame("facebok ");
+            frame.setContentPane(this.Backgrund);
+            frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+            frame.pack();
+            frame.setVisible(true);
 
-            name.addComponentListener(new ComponentAdapter() {
+            Backgrund.addComponentListener(new ComponentAdapter() {
                 @Override
                 public void componentResized(ComponentEvent componentEvent) {
                     super.componentResized(componentEvent);
                 }
             });
-        JFrame frame = new JFrame("facebok ");
-        frame.setContentPane(this.Backgrund);
-        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        frame.pack();
-        frame.setVisible(true);
+        }
     }
-    }
+
 
